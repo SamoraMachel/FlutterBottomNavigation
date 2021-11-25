206b18c9dd7b9f17a7daa526c92b4613ddc344b1
Samora Machel <samora@me.laptop>
2021-11-25 15:11:53 +0300

    Created a sample bottom navigation

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bottom_navigation/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tambua',
+      theme: ThemeData(
+        // This is the theme
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
